43f3a3ef38d87351d88def810cd68b24e224d456
Tao Wu <mouse.wu@sina.com>
2018-05-29 20:58:44 +0800

    init value

---
+++ src/main/java/com/thoughtworks/nho/nho29/Nho29Application.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.nho.nho29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nho29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nho29Application.class, args);
+	}
+}
