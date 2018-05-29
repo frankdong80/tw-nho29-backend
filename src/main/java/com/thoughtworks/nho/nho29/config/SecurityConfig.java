11473f2bc3859a37f49f36b031e527aee2f95be4
fqdong <frankdong80@gmail.com>
2018-05-29 21:33:32 +0800

    update

--- src/main/java/com/thoughtworks/nho/nho29/config/SecurityConfig.java
+++ src/main/java/com/thoughtworks/nho/nho29/config/SecurityConfig.java
@@ -1,21 +1,4 @@
-package com.blackbirdsport.ershou.config;
-
-import com.blackbirdsport.ershou.domain.AccountSecurityRepository;
-import com.blackbirdsport.ershou.domain.ClientRepository;
-import com.blackbirdsport.ershou.domain.ManagerRepository;
-import com.blackbirdsport.ershou.exception.EncryptionException;
-import com.blackbirdsport.ershou.utils.EncryptionTool;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
+package com.thoughtworks.nho.nho29.config;
 /**
  * @author Fuqiang
  * Created on 20/06/2017.
