232b1dad95bfe3ed0e21260e9bd0711be4377960
fqdong <frankdong80@gmail.com>
2018-05-30 14:25:30 +0800

    create TaskCardService

---
+++ src/main/java/com/thoughtworks/nho/nho29/service/TaskCardService.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.nho.nho29.service;
+
+import com.thoughtworks.nho.nho29.domain.TaskCard;
+
+public interface TaskCardService {
+
+    TaskCard getTaskCardById(Long id);
+}
---
+++ src/main/java/com/thoughtworks/nho/nho29/service/impl/TaskCardServiceImpl.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.nho.nho29.service.impl;
+
+import com.thoughtworks.nho.nho29.domain.TaskCard;
+import com.thoughtworks.nho.nho29.service.TaskCardService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TaskCardServiceImpl implements TaskCardService {
+
+    @Override
+    public TaskCard getTaskCardById(Long id) {
+
+        return null;
+    }
+}
