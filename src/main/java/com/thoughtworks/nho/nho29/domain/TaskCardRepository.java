7d4ce15f9cbadce39e3782f9437aaa2025b425ad
fhx0628 <564246125@qq.com>
2018-05-30 14:30:56 +0800

    add schema.sql and add taskCard Repo

---
+++ src/main/java/com/thoughtworks/nho/nho29/domain/TaskCardRepository.java
@@ -0,0 +1,6 @@
+package com.thoughtworks.nho.nho29.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskCardRepository extends JpaRepository<TaskCard,Long> {
+}
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,5 @@
 delete from nho_user;
 insert into nho_user values (0,'1','1');
+
+delete from training_club ；
+insert into values (1, '训练营1', '欢迎来到训练营1', '张三');
