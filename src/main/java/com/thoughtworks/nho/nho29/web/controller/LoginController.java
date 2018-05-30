ed6ad0ae9eef110319dd2c06097a5fbf589b2377
Tao Wu <mouse.wu@sina.com>
2018-05-30 11:52:56 +0800

    fix bugs

--- src/main/java/com/thoughtworks/nho/nho29/web/controller/LoginController.java
+++ src/main/java/com/thoughtworks/nho/nho29/web/controller/LoginController.java
@@ -2,9 +2,6 @@ package com.thoughtworks.nho.nho29.web.controller;
 
 import com.thoughtworks.nho.nho29.domain.User;
 import com.thoughtworks.nho.nho29.domain.UserRepository;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.SwaggerDefinition;
-import io.swagger.annotations.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
-@Api(tags = "Login Service")
-@SwaggerDefinition(tags = @Tag(
-        name = "Login",
-        description = "Login service"))
 @RestController
-@RequestMapping(value =  "/auth", produces = APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(value = "/auth", produces = APPLICATION_JSON_UTF8_VALUE)
 @Slf4j
 public class LoginController {
     @Autowired
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,1 +1,2 @@
+delete from nho_user;
 insert into nho_user values (0,'1','1');
