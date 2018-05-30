5062f9cb0a53d2dd0467eae2c4d7f5da0c53f4ac
fhx0628 <564246125@qq.com>
2018-05-30 22:29:28 +0800

    update test for TaskCardService

--- src/test/java/com/thoughtworks/nho/nho29/service/impl/TaskCardServiceImplTest.java
+++ src/test/java/com/thoughtworks/nho/nho29/service/impl/TaskCardServiceImplTest.java
@@ -5,10 +5,10 @@ import com.thoughtworks.nho.nho29.service.TaskCardService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
 
@@ -17,22 +17,25 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
+
 @SpringBootTest
 public class TaskCardServiceImplTest {
     @Mock
     TaskCardRepository repo;
 
+    @InjectMocks
+    private TaskCardServiceImpl taskCardServiceImpl;
 
-    @Autowired
-    TaskCardServiceImpl taskCardServiceImpl;
-
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
     @Test
-    public void test_getTaskCardListByTrainingClubId_while_id_is_correct () {
-
+    public void test_getTaskCardListByTrainingClubId_while_id_is_correct() {
         when(repo.findAllByTrainingClubId(anyLong())).thenReturn(Collections.emptyList());
 
-        Long trainingCLubId =  1l;
-        assertTrue(taskCardServiceImpl.getTaskCardListByTrainingClubId(trainingCLubId).size() >=0);
+        Long trainingCLubId = 1l;
+        assertTrue(taskCardServiceImpl.getTaskCardListByTrainingClubId(trainingCLubId).size() >= 0);
         verify(repo).findAllByTrainingClubId(eq(trainingCLubId));
         verifyNoMoreInteractions(repo);
     }
